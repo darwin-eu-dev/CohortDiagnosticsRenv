4243ec8318030d52300b41f3c91760553f3fb2a1
Cesar Barboza <c.barbozagutierrez@erasmusmc.nl>
2023-12-15 23:18:46 +0100

    package format

--- R/runDiagnostics.R
+++ R/runDiagnostics.R
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#' Execute the CohortDiagnostics section of the study.
+#' `runDiagnostics()` Execute the CohortDiagnostics section of the study.
 #'
 #' @details
 #' This function executes the cohort diagnostics.
